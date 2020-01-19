b3a9d9e8a3f579c2a2eb66966c6cec06a636733b
saranjothi <54699601+saranjothi@users.noreply.github.com>
2020-01-19 20:43:26 +0530

    Update training.java

--- Training/src/training.java
+++ Training/src/training.java
@@ -3,6 +3,6 @@ public class training {
 	public static void main(String args[]){  
 	     System.out.println("welcome Java");
 		System.out.println(" hi tom");
-		System.out.println("Java");
+		System.out.println("Java hiii");
 	    }  
 }
